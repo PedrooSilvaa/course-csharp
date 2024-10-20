7ac430de24117f7a0c817305608601cc9de88db6
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-10-20 19:19:33 -0300

    feat: data entry class part 1

---
+++ EntradaDados1/EntradaDados1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            //Bom dia! - Vermelho - Verde - Azul - Amarelo Laranja Cinza
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(s);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
